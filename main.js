8729e2d26dfd0d114a5734e5221c7b55929fa978
cbretheim <33535137+cbretheim@users.noreply.github.com>
2020-07-25 10:16:36 -0500

    About, Process pages
    Including column formatting

---
+++ main.js
@@ -0,0 +1,14 @@
+/*attempt to change nav color when scrolling
+var myNav = document.getElementById('main-nav');
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    }
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+*/
